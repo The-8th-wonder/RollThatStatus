ff140d5f87c47174b25045bd701b9e5c398a0459
JuliaCaesar <101206316+JuliaCaesar@users.noreply.github.com>
2025-05-10 13:19:30 -0500

    i broke it :(

--- roll-that-status/src/index.js
+++ roll-that-status/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import BasicModal from './Modal';
+// import BasicModal from './Modal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-root.render(<BasicModal />);
+// root.render(<BasicModal />);
 
 
 
