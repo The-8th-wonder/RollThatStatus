d2bc4d3f1834f79d1078d5b0a7e9a0de628eae09
JuliaCaesar <101206316+JuliaCaesar@users.noreply.github.com>
2025-05-10 12:50:02 -0500

    Reapply "quick clean up"
    This reverts commit b5d0867fc814228c771a9f2095eeb6b3c2041307.

--- roll-that-status/src/index.js
+++ roll-that-status/src/index.js
@@ -5,8 +5,9 @@ import App from './App';
 import BasicModal from './Modal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
 root.render(<BasicModal />);
+root.render(<App />);
+
 
 
 
