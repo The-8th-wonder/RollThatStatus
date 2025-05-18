4594f8732c715646108fe58e57edf3fc95be2a04
JuliaCaesar <101206316+JuliaCaesar@users.noreply.github.com>
2025-05-17 22:05:46 -0500

    Update StatusOfTheDay.js

--- roll-that-status/src/StatusOfTheDay.js
+++ roll-that-status/src/StatusOfTheDay.js
@@ -16,7 +16,7 @@ export default function StatusOfTheDay(){
 
 function Tab() {
 
-    const text = "Why are the fanfics I read so depressing? 💀";
+    const text = "I will forever take the burdern\nas no one wants to take mine";
 
     const[isOpen, setIsOpen] = React.useState(false);
     const[showBadge, setshowBadge] = React.useState(true);
