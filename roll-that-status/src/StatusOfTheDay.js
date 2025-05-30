2268318eff713f90c8bbf5a56dbc713df3ceac84
JuliaCaesar <101206316+JuliaCaesar@users.noreply.github.com>
2025-05-29 21:04:10 -0500

    Update StatusOfTheDay.js

--- roll-that-status/src/StatusOfTheDay.js
+++ roll-that-status/src/StatusOfTheDay.js
@@ -16,7 +16,7 @@ export default function StatusOfTheDay(){
 
 function Tab() {
 
-    const text = "Your love is as bright as the starlight";
+    const text = "I want to live in a Star Trek society";
 
     const[isOpen, setIsOpen] = React.useState(false);
     const[showBadge, setshowBadge] = React.useState(true);
