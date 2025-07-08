3867200df5588033c06ffbc86973d6c7c5177c4c
Mads <101206316+JuliaCaesar@users.noreply.github.com>
2025-07-08 17:51:40 -0500

    Update StatusOfTheDay.js

--- roll-that-status/src/StatusOfTheDay.js
+++ roll-that-status/src/StatusOfTheDay.js
@@ -17,7 +17,7 @@ export default function StatusOfTheDay(){
 function Tab() {
 
   // Status of the day text
-  const text = "is this what it feels like to belong?";
+  const text = "i'm so severely lonely\nwill i find the strength to save myself?";
 
   const[isOpen, setIsOpen] = React.useState(false);
 
