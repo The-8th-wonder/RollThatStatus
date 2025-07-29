bf42a121258e0f640322f8fc5fa8ab5b405a11f1
Mads <101206316+JuliaCaesar@users.noreply.github.com>
2025-07-29 15:07:00 -0500

    Update StatusOfTheDay.js

--- roll-that-status/src/StatusOfTheDay.js
+++ roll-that-status/src/StatusOfTheDay.js
@@ -17,7 +17,7 @@ export default function StatusOfTheDay(){
 function Tab() {
 
   // Status of the day text
-  const text = "Kill me quick, kill me fast.";
+  const text = "Rolling with the punches until i die";
 
   const[isOpen, setIsOpen] = React.useState(false);
 
