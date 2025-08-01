e6069bd1a7f004796521a4814109a285a34cc5f3
Mads <101206316+JuliaCaesar@users.noreply.github.com>
2025-08-01 10:10:58 -0500

    Update Announcements.js

--- roll-that-status/src/Announcements.js
+++ roll-that-status/src/Announcements.js
@@ -15,7 +15,7 @@ export default function Announcement(){
 
 function AnnouncementTab() {
 
-    const text = "This is on hiatus cause i got addicted to balatro :\(";
+    const text = "Updates coming soon!!!";
 
     const[isOpen, setIsOpen] = React.useState(false);
 
