717539527b722436bd1c77057aa5cf61e8ed3c87
Mads <101206316+The-8th-wonder@users.noreply.github.com>
2026-01-15 11:29:06 -0600

    Update App.js

--- roll-that-status/src/App.js
+++ roll-that-status/src/App.js
@@ -9,7 +9,7 @@ export default function App(){
             <RunStatus />
             <StatusOfTheDay />
             <PokerChip />
-            <Announcement />
+            //<Announcement />
         </section>
     )
 }
