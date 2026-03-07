703fe779daf779df2904e5527ce2b6ab0b94a421
Mads <101206316+The-8th-wonder@users.noreply.github.com>
2026-03-06 18:08:18 -0600

    Update App.jsx

--- roll-that-status/src/App.jsx
+++ roll-that-status/src/App.jsx
@@ -2,6 +2,7 @@ import RunStatus from './Status';
 import StatusOfTheDay from './StatusOfTheDay';
 import PokerChip from './PokerChip';
 import ImaComputer from './ImaComputer';
+import Announcements from './Announcements';
 
 export default function App(){
     return(
@@ -10,6 +11,7 @@ export default function App(){
             <StatusOfTheDay />
             <PokerChip />
             <ImaComputer />
+            <Announcements />
         </section>
     )
 }
