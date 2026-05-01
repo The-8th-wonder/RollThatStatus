d1e639667f02d0a5c159c2e26a8379b0ed6acbdd
Mads <101206316+The-8th-wonder@users.noreply.github.com>
2026-05-01 12:26:03 -0500

    Update StatusOfTheDay.jsx

--- roll-that-status/src/StatusOfTheDay.jsx
+++ roll-that-status/src/StatusOfTheDay.jsx
@@ -17,7 +17,7 @@ export default function StatusOfTheDay(){
 function Tab() {
 
   // Status of the day text
-  const text = "If you wanna soar with vultures\nyou\'ll have to swallow bone";
+  const text = "when is it my turn to be happy";
 
   const[isOpen, setIsOpen] = React.useState(false);
 
