7c3c339027f31236f308da5311364e1dcdd6d5f1
Mads <101206316+The-8th-wonder@users.noreply.github.com>
2026-06-24 10:10:36 -0500

    Update App.jsx

--- roll-that-status/src/App.jsx
+++ roll-that-status/src/App.jsx
@@ -8,6 +8,7 @@ export default function App(){
     return(
         <section>
             <RunStatus />
+            <Announcement />
             <StatusOfTheDay />
             <PokerChip />
             <ImaComputer />
